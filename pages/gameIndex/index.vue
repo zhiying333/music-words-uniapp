c8bf9040f9c31194194ee67e609bd575e483c3cc
zhiying <fordustsanny@outlook.com>
2021-07-16 12:18:08 +0800

    初始化仓库

---
+++ pages/gameIndex/index.vue
@@ -0,0 +1,64 @@
+<template>
+	<view class="content">
+		<image class="logo" src="../../static/logo.png" mode=""></image>
+		<view class="play-cate">
+			<view class="cate-item man-machine">
+				人机接龙
+			</view>
+			<view class="cate-item on-line">
+				歌词联机
+			</view>
+		</view>
+		<view class="welcome">
+			From 三三 with Love ❤
+		</view>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				
+			}
+		},
+		onLoad() {
+
+		},
+		methods: {
+
+		}
+	}
+</script>
+
+<style lang="less">
+	uni-page-body {
+		height: 100%;
+	}
+	.content {
+		position: relative;
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		height: 100%;
+		.logo {
+			margin: 20rpx;
+		}
+		.play-cate {
+			.cate-item {
+				width: 275rpx;
+				height: 100rpx;
+				line-height: 100rpx;
+				background-color: skyblue;
+				margin: 20rpx;
+				text-align: center;
+				border-radius: 20rpx;
+				color: #f00;
+			}
+		}
+		.welcome {
+			position: absolute;
+			bottom: 30rpx;
+		}
+	}
+</style>
---
+++ pages/mine/mine.vue
@@ -0,0 +1,19 @@
+<template>
+	<view>
+		个人中心页
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				
+			};
+		}
+	}
+</script>
+
+<style lang="less">
+
+</style>
